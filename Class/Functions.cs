3a7687897c66cbb3b29094b3e6d59809093dbe61
nhuluc78 <119469132+nhuluc78@users.noreply.github.com>
2022-11-30 10:59:06 +0700

    aaaaa

--- Class/Functions.cs
+++ Class/Functions.cs
@@ -12,19 +12,17 @@ using System.Drawing;
 namespace QLBN.Class
 {
     class Functions
-    {
+    {       
         public static SqlConnection Con;  //Khai báo đối tượng kết nối        
-
         public static void Connect()
         {
             Con = new SqlConnection();   //Khởi tạo đối tượng
-            Con.ConnectionString = @"Data Source=DESKTOP-9DG1GEJ;Initial Catalog=QLBN_2;Integrated Security=True";
+            Con.ConnectionString = @"Data Source=.\SQLEXPRESS;AttachDbFilename=" + Application.StartupPath + @"\QLBN_DB.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True";
             Con.Open();                  //Mở kết nối
             //Kiểm tra kết nối
             if (Con.State == ConnectionState.Open)
                 MessageBox.Show("Kết nối thành công");
             else MessageBox.Show("Không thể kết nối với dữ liệu");
-
         }
         public static void Disconnect()
         {
@@ -39,13 +37,13 @@ namespace QLBN.Class
         //Lấy dữ liệu vào bảng
         public static DataTable GetDataToTable(string sql)
         {
+            DataTable table = new DataTable();
             SqlDataAdapter dap = new SqlDataAdapter(); //Định nghĩa đối tượng thuộc lớp SqlDataAdapter
             //Tạo đối tượng thuộc lớp SqlCommand
             dap.SelectCommand = new SqlCommand();
             dap.SelectCommand.Connection = Functions.Con; //Kết nối cơ sở dữ liệu
             dap.SelectCommand.CommandText = sql; //Lệnh SQL
             //Khai báo đối tượng table thuộc lớp DataTable
-            DataTable table = new DataTable();
             dap.Fill(table);
             return table;
         }
